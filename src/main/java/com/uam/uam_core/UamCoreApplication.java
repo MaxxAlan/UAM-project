3c5eeeef3f31a803a1f7278d6297d22bbf517941
Hoang Manh <95678367+MaxxAlan@users.noreply.github.com>
2026-02-24 11:16:40 +0700

    Add files via upload

---
+++ src/main/java/com/uam/uam_core/UamCoreApplication.java
@@ -0,0 +1,13 @@
+package com.uam.uam_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UamCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UamCoreApplication.class, args);
+	}
+
+}
